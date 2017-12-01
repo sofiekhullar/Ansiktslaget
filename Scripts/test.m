8edab2216789285f19857ffc01824ad687725fbb
sofiekhullar <sofie_k@live.se>
2017-12-01 08:58:47 +0100

    started with eigenfaces

--- Scripts/test.m
+++ Scripts/test.m
@@ -45,6 +45,49 @@ end
 figure;
 imshow(mat2gray(norm_image))
 
+%%
+gammaArray = {}; 
+psiArray = {};
+
+M = length(newImageArray);
+norm_image_vector = norm_image(:);
+
+% step 2 - Represent image as vector
+for i = 1:M
+    currentTestImg = newImageArray{i};
+    currentTestImg = im2double(currentTestImg);
+    gammaArray{i} = currentTestImg(:);
+end
+
+M2 = length(gammaArray{1,1});
+sumVector = zeros(M2, 1);
+
+% step 3 - Find the average face vector psi
+for i = 1:M
+    tmpVector = gammaArray{1,i};
+    for j = 1:M2
+        sumVector(j,1) = (sumVector(j,1) + tmpVector(j,1));
+    end
+end
+psi = 1/M .* sumVector;
+
+% step 4 - Subtract the mean face from each face vector 
+for i = 1:M
+    phi{i} = gammaArray{1,i} - norm_image_vector;
+end
+
+%% step 5 - Find the Covariance matrix C
+% A = [phi1, phi2, ....]
+% C = A A^T
+% A is of size n x M
+% C is a N^2 times N^2 
+
+A = cell2mat(phi);
+AT = A.'; 
+C = AT*A;
+
+%C = cov(A, AT);
+
 %% Call function tnm034 
 
 id = tnm034(imageArray{1})
