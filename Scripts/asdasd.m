376ba62c75a265dd47a720bb651e3afd1ce7eb40
Love Lidberg <lovli064@student.liu.se>
2017-12-04 11:26:02 +0100

    haha hej

--- Scripts/asdasd.m
+++ Scripts/asdasd.m
@@ -1,14 +1,26 @@
+DBdir = '../Images/DB2/';
+imagefiles = dir(strcat(DBdir,'*.jpg')); 
+nfiles = length(imagefiles);
+imageArray = {1,nfiles}; 
+
+for i = 1:nfiles
+   currentfilename = imagefiles(i).name;
+   currentimage = imread(strcat(DBdir,currentfilename));
+   imageArray{i} = currentimage;
+end
+
 for i = 1:16
     
     test = imageArray{i};
     face = findFace(test);
     masked = bsxfun( @times, test, cast(face, class(test)) );
 
-%     imshow(test);
+    imshow(test);
+    pause;
+    imshow(masked);
+    pause;
 %     pause;
-%     imshow(masked);
-%     pause;
-    newEye(masked);
+%     newEye(masked);
 %     [e1, e2] = findEyes(masked);
 %     m = findMouth(masked);
 %     eyes = insertMarker(test,[e1(1) e1(2); e2(1) e2(2)]);
