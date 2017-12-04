1c3f9e463094558a11889deb270c083a3b1ef6b1
Love Lidberg <lovli064@student.liu.se>
2017-12-04 10:47:13 +0100

    distance function added

---
+++ Scripts/findIndex.m
@@ -0,0 +1,18 @@
+function [min_ind] = findIndex(w, x) 
+
+    threshold = 10000;
+    n_images = size(w,2);
+    min_dist = 10000;
+    min_ind = 0;
+
+    for i = 1:n_images
+        dist = sqrt((w(:,i) - x).^2);
+        
+        if  dist < min_dist
+            min_dist = dist;
+            if min_dist < threshold
+                min_ind = i;
+            end
+        end
+    end
+end
