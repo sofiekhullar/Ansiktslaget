76a2c3bf8b24cfbe710997f41fc3c09122ce5fd5
Love Lidberg <lovli064@student.liu.se>
2017-12-08 16:09:40 +0100

    Put variables in mat-file and changed tnm034.m so it loads those variables

--- Scripts/test.m
+++ Scripts/test.m
@@ -10,8 +10,22 @@ for i=1:nfiles
    imageArray{i} = currentimage;
 end
 
-%% Read in input image
-input_img = imread('../Images/DB0/db0_3.jpg');
+images = imageArray;
+%%
+correct = [1 2 4 5 6 7 10 13 14 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 1 3 4 7 9 11 12 1 7 8 9 12 16]';
+% correct = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16]';
+% correct = [0 0 0 0]';
 
-%% Call function tnm034 
-id = tnm034(imageArray{2})
+score = zeros(16,10);
+% 
+% for k = 1:16
+%     for threshold = 200:200:2000
+        prediction = zeros(length(images),1);
+        for i = 1:length(images)
+            prediction(i) = tnm034(images{i}, 10, 200)
+        end
+        score1 =  mean(prediction == correct)
+%         score(k, threshold/200) = mean(prediction == correct);
+%         score(k, threshold/200)
+%     end
+% end
--- Scripts/tnm034.m
+++ Scripts/tnm034.m
@@ -1,96 +1,112 @@
-function [ id ] = tnm034( img )
-
+function [ id ] = tnm034( img, k, threshold)
+load('variables.mat');
 % Read in data from DB1 and save in imageArray
-DBdir = '../Images/DB1/';
-imagefiles = dir(strcat(DBdir,'*.jpg')); 
-nfiles = length(imagefiles);
-imageArray = {1,nfiles}; 
-
-for i=1:nfiles
-   currentfilename = imagefiles(i).name;
-   currentimage = imread(strcat(DBdir,currentfilename));
-   imageArray{i} = currentimage;
-end
+% DBdir = '../Images/DB1/';
+% imagefiles = dir(strcat(DBdir,'*.jpg')); 
+% nfiles = length(imagefiles);
+% imageArray = {1,nfiles}; 
+% 
+% for i=1:nfiles
+%    currentfilename = imagefiles(i).name;
+%    currentimage = imread(strcat(DBdir,currentfilename));
+%    imageArray{i} = currentimage;
+% end
 
 % Calc new image array
-newnfiles = 0;
-newImageArray = {1,nfiles};
-
-for i=1:nfiles
-    currentImage = imageArray{i};
-    [leftEye, rightEye] = findEyes(currentImage);
-    mouth = findMouth(currentImage);
-    
-    if (isnan(leftEye(1,1)) || isnan(leftEye(1,2)) || isnan(rightEye(1,1)) || isnan(rightEye(1,2)) || isnan(mouth(1,1)) || isnan(mouth(1,2)) || (i == 12))
-        continue;
-    end
-    
-    newnfiles = newnfiles + 1;
-    
-    currentImage = transformFace(currentImage, leftEye, rightEye, mouth);
-    newImage = rgb2gray(currentImage);
-    
-    newImageArray{newnfiles} = newImage;
-end 
+% newnfiles = 0;
+% newImageArray = {1,nfiles};
+% 
+% for i=1:nfiles
+%     currentImage = imageArray{i};
+%     [leftEye, rightEye] = findEyes(currentImage);
+%     mouth = findMouth(currentImage);
+%     
+%     if (isnan(leftEye(1,1)) || isnan(leftEye(1,2)) || isnan(rightEye(1,1)) || isnan(rightEye(1,2)) || isnan(mouth(1,1)) || isnan(mouth(1,2)) )
+%         continue;
+%     end
+%     
+%     newnfiles = newnfiles + 1;
+%     
+%     currentImage = transformFace(currentImage, leftEye, rightEye, mouth);
+%     newImage = rgb2gray(currentImage);
+%     
+%     newImageArray{newnfiles} = newImage;
+% end 
 
 % Norm Image
-clc
-size = length(newImageArray{1});
-norm_image = zeros(size, size);
+% clc
+% size = length(newImageArray{1});
+% norm_image = zeros(size, size);
+% 
+% for i = 1:length(newImageArray)
+%     norm_image = norm_image + mat2gray(newImageArray{i});
+% end
 
-for i = 1:length(newImageArray)
-    norm_image = norm_image + mat2gray(newImageArray{i});
-end
 
 % Norm input image
 [leftEye_input, rightEye_input] = findEyes(img);
 mouth_input = findMouth(img);
+
+if (isnan(leftEye_input(1,1)) || isnan(leftEye_input(1,2)) || isnan(rightEye_input(1,1)) || isnan(rightEye_input(1,2)) || isnan(mouth_input(1,1)) || isnan(mouth_input(1,2)) )
+        id = 0;
+        return;
+end
 input_img = transformFace(img, leftEye_input, rightEye_input, mouth_input);
 input_img = rgb2gray(input_img);
 
 % Calc eigenfaces
-gammaArray = {}; 
-
-M = length(newImageArray);
-norm_image_G = mat2gray(norm_image);
-norm_image_vector = norm_image_G(:);
-
+% gammaArray = {}; 
+% 
+% M = length(newImageArray);
+% norm_image_G = mat2gray(norm_image);
+% norm_image_vector = norm_image_G(:);
 
 input_img_vector = im2double(input_img(:));
 
 % step 2 - Represent image as vector
-for i = 1:M
-    currentTestImg = newImageArray{i};
-    currentTestImg = im2double(currentTestImg);
-    gammaArray{i} = currentTestImg(:);
-end
+% for i = 1:M
+%     currentTestImg = newImageArray{i};
+%     currentTestImg = im2double(currentTestImg);
+%     gammaArray{i} = currentTestImg(:);
+% end
 
-N2 = length(gammaArray{1,1});
-sumVector = zeros(N2, 1);
+% N2 = length(gammaArray{1,1});
+% sumVector = zeros(N2, 1);
 
 % step 3 - Find the average face vector psi
-psi = norm_image_vector;
+% psi = norm_image_vector;
 
 % step 4 - Subtract the mean fae from each face vector 
-phi = {};
-for i = 1:M
-    phi{i} = gammaArray{1,i} - psi;
-end
+% phi = {};
+% for i = 1:M
+%     phi{i} = gammaArray{1,i} - psi;
+% end
 
+if (size(input_img_vector,1)) ~= (size(norm_image_vector,1))
+    id = 0;
+    imshow(input_img);
+    pause;
+    disp('hej');
+    return;
+end
+size(input_img_vector)
+size(norm_image_vector)
 input_img_vector = input_img_vector - norm_image_vector;
 
-% step 5 - Find the Covariance matrix C
-A = cell2mat(phi);
-C = A'*A;
+% imshow(mat2gray(reshape(input_img_vector, [261 261])));
 
-[eigenVectors, eigenValues] = eig(C);
+% step 5 - Find the Covariance matrix C
+% A = cell2mat(phi);
+% C = A'*A;
+% 
+% [eigenVectors, eigenValues] = eig(C);
 
 %[M, I] = max(max(eigenValues));
-k = 6;
- 
-% To sort the eigenvectors with the eigenvalues
-[L, ind] = sort(diag(eigenValues),'descend');
-sortedEigenVectors = eigenVectors(:, ind);
+% k = 14;
+
+% % To sort the eigenvectors with the eigenvalues
+% [L, ind] = sort(diag(eigenValues),'descend');
+% sortedEigenVectors = eigenVectors(:, ind);
 
 v = sortedEigenVectors(:,1:k);
 u = A * v;
@@ -104,7 +120,14 @@ w = u' * A;
 % for input image
 w_input = u' * input_img_vector;
 
-id = findIndex(w, w_input);
+id = findIndex(w, w_input, threshold);
+
+% variables = matfile('variables.mat', 'Writable', true);
+% variables.u = u;
+% variables.w = w;
+% variables.A = A;
+% variables.norm_image_vector = norm_image_vector;
+% variables.sortedEigenVectors = sortedEigenVectors;
 
 end
 
